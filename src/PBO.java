7205789fa47501cb947d4f927408c510f9960bf6
DestaAri1 <72483038+DestaAri1@users.noreply.github.com>
2021-10-05 08:01:58 +0700

    Add files via upload

---
+++ src/PBO.java
@@ -0,0 +1,16 @@
+public class PBO {
+    int spidol, a;
+    int pulpen, b;
+    int pensil, c;
+    int penghapus_spidol, d;
+    int tipe_X, e;
+    int penghapus, f;
+    int penggaris, g;
+    int total_beli;
+    int total_bayar;
+    int total;
+    int bayar;
+    int kembalian;
+    int kurang;
+
+}
