a83ad13ea71de13e71c7e0cb544e5668c074cc7f
DestaAri1 <72483038+DestaAri1@users.noreply.github.com>
2021-10-10 21:10:25 +0700

    Add files via upload

--- src/PBO.java
+++ src/PBO.java
@@ -1,16 +1,56 @@
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+
 public class PBO {
-    int spidol, a;
+    int spidol_biasa, a;
     int pulpen, b;
     int pensil, c;
-    int penghapus_spidol, d;
-    int tipe_X, e;
-    int penghapus, f;
-    int penggaris, g;
+    int spidol_papan_tulis, d;
+    int penghapus_spidol_papan, e;
+    int tipe_X, f;
+    int penghapus, g;
+    int penggaris, h;
     int total_beli;
-    int total_bayar;
+    int total_belanja;
     int total;
     int bayar;
     int kembalian;
     int kurang;
+    String nama, jawab, order;
 
+    Scanner input = new Scanner(System.in);
+
+        public static void main(String[] args) {
+        PBO object = new PBO();
+    }
+
+void transaksi(){
+    spidol_biasa = 3500;
+    pulpen = 2500;
+    pensil = 2000;
+    spidol_papan_tulis = 6000;
+    penghapus_spidol_papan = 5500;
+    tipe_X = 5000;
+    penghapus = 1000;
+    penggaris = 2000;
+    total_belanja = 0;
+    jawab = "N";
+
+    System.out.println("=====================");
+    System.out.println("===Toko Alat Tulis===");
+    System.out.println("=====================");
+    System.out.println("Masukkan nama : ");
+    nama = input.next();
+    do {
+        System.out.println("List Barang : ");
+        System.out.println("1. Spidol               = Rp. 3500");
+        System.out.println("2. Pulpen               = Rp. 2500");
+        System.out.println("3. Pensil               = Rp. 2000");
+        System.out.println("4. Spidol Whiteboard    = Rp. 6000");
+        System.out.println("5. Penghapus Whiteboard = Rp. 5500");
+        System.out.println("6. Tip X                = Rp. 5000");
+        System.out.println("7. Penghapus            = Rp. 1000");
+        System.out.println("8. Penggaris            = Rp. 2000");
+        System.out.println("Pilih Produk : ");
+}
 }
